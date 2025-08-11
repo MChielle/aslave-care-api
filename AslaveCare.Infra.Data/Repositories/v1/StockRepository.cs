a19c840d807411d87697baf3052f7a02f58a48a3
Matheus Pires Chielle <matheuschielle@gmail.com>
2025-08-11 20:22:55 -0300

    Recount.

--- AslaveCare.Infra.Data/Repositories/v1/StockRepository.cs
+++ AslaveCare.Infra.Data/Repositories/v1/StockRepository.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using AslaveCare.Domain.Entities;
+﻿using AslaveCare.Domain.Entities;
 using AslaveCare.Domain.Interfaces.Repositories.v1;
 using AslaveCare.Domain.Models.v1.Stock;
 using AslaveCare.Infra.Data.Context;
 using AslaveCare.Infra.Data.Context.RepositoryContext;
 using AslaveCare.Infra.Data.Repositories.Base;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AslaveCare.Infra.Data.Repositories.v1
 {
@@ -95,34 +95,123 @@ namespace AslaveCare.Infra.Data.Repositories.v1
 
         public async Task<bool> RecountAsync(List<Stock> stocksToRecount, CancellationToken cancellationToken = default)
         {
-            var stocks = await _context.Stocks
-                .Include(x => x.RegisterInStocks)
-                    .ThenInclude(x => x.RegisterIn)
-                .Include(x => x.RegisterOutStocks)
-                    .ThenInclude(x => x.RegisterOut)
-                .Where(x => stocksToRecount.Any(y => y.Id.Equals(x.Id)) && x.DeletionDate.Equals(null))
-                .Select(x => new KeyValuePair<Guid, decimal>(x.Id,
-                    x.RegisterInStocks.Where(x => x.RegisterIn.DeletionDate.Equals(null) && x.RegisterIn.Apply).Sum(x => x.Quantity)
-                    -
-                    x.RegisterOutStocks.Where(x => x.RegisterOut.DeletionDate.Equals(null) && x.RegisterOut.Apply).Sum(x => x.Quantity))
-                )
-                .Where(x => x.Value != 0)
-                .ToListAsync(cancellationToken);
+            //var stocksToUpdate = await _context.Stocks.Where(x => stocksToRecount.Select(w => w.Id).Contains(x.Id)).ToListAsync();
+            //foreach (var item in stocksToUpdate)
+            //{
+            //    stock.Quantity = item.Quantity;
+            //    stock.LastChangeDate = DateTime.UtcNow;
+            //}
+            //await _context.Stocks.ExecuteUpdateAsync(stock);
 
-            return true;
+            //var recountStocks = await _context.Stocks
+            //    .Include(x => x.RegisterInStocks)
+            //        .ThenInclude(x => x.RegisterIn)
+            //    .Include(x => x.RegisterOutStocks)
+            //        .ThenInclude(x => x.RegisterOut)
+            //    .Where(x => stocksToRecount.Select(w => w.Id).Contains(x.Id) && x.DeletionDate.Equals(null))
+            //    .Select(x => new
+            //    {
+            //        x.Id,
+            //        // Quantidade recontada no depósito
+            //        ActualQuantity = x.Quantity,
+            //        // Calculo da quantidade registrada por movimentação
+            //        CalculatedQuantity = x.RegisterInStocks.Where(x => x.RegisterIn.DeletionDate.Equals(null) && x.RegisterIn.Apply).Sum(x => x.Quantity)
+            //        -
+            //        x.RegisterOutStocks.Where(x => x.RegisterOut.DeletionDate.Equals(null) && x.RegisterOut.Apply).Sum(x => x.Quantity),
+            //    })
+            //    //Ignora os que estão com quantidade correta.
+            //    .Select(x => new { x.Id, x.ActualQuantity, x.CalculatedQuantity, Diference = Math.Abs(x.CalculatedQuantity - x.ActualQuantity) })
+            //    .Where(x => x.Diference != 0)
+            //    .ToListAsync(cancellationToken);
 
-            //await _context.Database.BeginTransactionAsync(cancellationToken);
+            //if (recountStocks.Count == 0)
+            //    return true;
 
-            //Receber a lista de itens a serem recalculados com seus saldos atuais. OK
-            //Ver se o saldo calculado esta acima ou abaixo. OK
-            //Criar registro de entrada para os itens com saldo acima.
+            //var lastRegisterInNumber = 0;
+            //RegisterIn registerIn = null;
+            //if (recountStocks.Any(x => x.Diference < 0))
+            //{
+            //    lastRegisterInNumber = await _context.RegisterInStocks
+            //        .Select(x => x.RegisterIn.Number)
+            //        .DefaultIfEmpty(0)
+            //        .MaxAsync(cancellationToken);
 
-            //Criar registro de saida para os itens com saldo abaixo.
-            
-            
-            //await _context.SaveChangesAsync(cancellationToken);
+            //    registerIn = new RegisterIn
+            //    {
+            //        Apply = true,
+            //        ApplyDate = new DateTime(2025, 5, 1),
+            //        CreationDate = new DateTime(2025, 5, 1),
+            //        DeletionDate = null,
+            //        Description = "Recontagem de estoque",
+            //        SupplierId = Guid.Parse("d7cc0743-6bd1-44d3-a087-5475097bcf7e"),
+            //        Donation = false,
+            //        Number = lastRegisterInNumber + 1,
+            //    };
 
-            //await _context.Database.CommitTransactionAsync(cancellationToken);
+            //    foreach (var stock in recountStocks.Where(x => x.Diference < 0))
+            //    {
+            //        registerIn.RegisterInStocks.Add(new RegisterInStock
+            //        {
+            //            StockId = stock.Id,
+            //            Quantity = stock.Diference * -1,
+            //            Price = 0,
+            //        });
+            //    }
+            //}
+
+            //var lastRegisterOutNumber = 0;
+            //RegisterOut registerOut = null;
+            //if (recountStocks.Any(x => x.Diference > 0))
+            //{
+            //    lastRegisterOutNumber = await _context.RegisterInStocks
+            //        .Select(x => x.RegisterIn.Number)
+            //        .DefaultIfEmpty(0)
+            //        .MaxAsync(cancellationToken);
+
+            //    registerOut = new RegisterOut
+            //    {
+            //        Apply = true,
+            //        ApplyDate = new DateTime(2025, 5, 1),
+            //        CreationDate = new DateTime(2025, 5, 1),
+            //        DeletionDate = null,
+            //        Description = "Recontagem de estoque",
+            //        Number = lastRegisterOutNumber + 1,
+            //    };
+
+            //    foreach (var stock in recountStocks.Where(x => x.Diference < 0))
+            //    {
+            //        registerOut.RegisterOutStocks.Add(new RegisterOutStock
+            //        {
+            //            StockId = stock.Id,
+            //            Quantity = stock.Diference,
+            //        });
+            //    }
+            //}
+
+            //try
+            //{
+            //    await _context.Database.BeginTransactionAsync(cancellationToken);
+
+            //    if (registerIn != null)
+            //    {
+            //        var result = await _context.AddAsync(registerIn);
+            //    }
+
+            //    if (registerOut != null)
+            //    {
+            //        var result = _context.RegistersOut.AddAsync(registerOut);
+            //    }
+
+            //    await _context.SaveChangesAsync(cancellationToken);
+            //    await _context.Database.CommitTransactionAsync(cancellationToken);
+
+                return true;
+            //}
+            //catch
+            //{
+            //    await _context.Database.RollbackTransactionAsync(cancellationToken);
+            //    return false;
+            //}
         }
     }
 }
